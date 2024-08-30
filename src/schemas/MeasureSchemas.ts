3982e3e155bcadd9215549cf017dd71bae53a3b6
Breno <breno_mg10@hotmail.com>
2024-08-29 21:25:32 -0300

    fix: base65 schema

--- src/schemas/MeasureSchemas.ts
+++ src/schemas/MeasureSchemas.ts
@@ -1,8 +1,22 @@
 import Joi from "joi";
 import { attMeasure, uploadType } from "protocols/measure";
 
+const isBase64 = (value: string) => {
+  const base64Regex = /^[A-Za-z0-9+/]+[=]{0,2}$/;
+  return base64Regex.test(value);
+};
+
 export const uploadSchema = Joi.object<uploadType>({
-  image: Joi.string().base64().required(),
+  image: Joi.string()
+    .replace(/\s/g, "")
+    .replace(/^data:image\/\w+;base64,/, "")
+    .custom((value, helpers) => {
+      if (!isBase64(value)) {
+        return helpers.error("any.invalid");
+      }
+      return value;
+    })
+    .required(),
   customer_code: Joi.string().required(),
   measure_datetime: Joi.date().required(),
   measure_type: Joi.string().valid("WATER", "GAS").required(),
